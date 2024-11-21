49eb6118974ac23f4c99db4c10616ad23d9f362c
NINOVEE <109622508+NinoVee@users.noreply.github.com>
2024-11-21 13:02:20 -0800

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,9 @@ import { FAKE_TOKEN_MINT, PoolToken, TokenMeta, makeHeliusTokenFetcher } from 'g
 export const RPC_ENDPOINT = import.meta.env.VITE_RPC_ENDPOINT ?? 'https://api.mainnet-beta.solana.com'
 
 // Solana address that will receive fees when somebody plays on this platform
-export const PLATFORM_CREATOR_ADDRESS = 'EXjZJegm5i4mESkVmPDhcxvdvTrXbFERnPtYrga9jAUf'
+export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
+  'V2grJiwjs25iJYqumbHyKo5MTK7SFqZSdmoRaj8QWb9',
+)
 
 // Gamba explorer URL - Appears in RecentPlays
 export const EXPLORER_URL = 'https://explorer.gamba.so'
@@ -14,10 +16,10 @@ export const EXPLORER_URL = 'https://explorer.gamba.so'
 export const PLATFORM_SHARABLE_URL = 'play.gamba.so'
 
 // Creator fee (in %)
-export const PLATFORM_CREATOR_FEE = 0.05 // 1% (1/100 = 0.01)  !!max 5%!!
+export const PLATFORM_CREATOR_FEE = 0.01 // 1% (1/100 = 0.01)  !!max 5%!!
 
 // Jackpot fee (in %)
-export const PLATFORM_JACKPOT_FEE = 0.005 // 0.1% (0.1/100 = 0.001)
+export const PLATFORM_JACKPOT_FEE = 0.001 // 0.1% (0.1/100 = 0.001)
 
 // Just a helper function
 const lp = (tokenMint: PublicKey | string, poolAuthority?: PublicKey | string): PoolToken => ({
@@ -37,8 +39,8 @@ export const POOLS = [
   lp('So11111111111111111111111111111111111111112'),
   // USDC:
   lp('EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v'),
-  // ACRES:
-  lp(''),
+  // Wormhole:
+  lp('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
 ]
 
 // The default token to be selected
@@ -51,18 +53,18 @@ export const DEFAULT_POOL = POOLS[0]
 export const TOKEN_METADATA: (Partial<TokenMeta> & {mint: PublicKey})[] = [
   {
     mint: FAKE_TOKEN_MINT,
-    name: 'TEST ACRES',
-    symbol: ' TEST ACRES',
+    name: 'Fake',
+    symbol: 'FAKE',
     image: '/fakemoney.png',
     baseWager: 1e9,
     decimals: 9,
     usdPrice: 0,
   },
   {
-    mint: new PublicKey('9hTF4azRpZQFqgZ3YpgACD3aSbbB4NkeEUhp7NKZvmWe'),
-    name: 'ACRES',
-    symbol: 'ACRES',
-    image: '/MLK LOGO.JPG',
+    mint: new PublicKey('85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ'),
+    name: 'W',
+    symbol: 'Wormhole',
+    image: 'https://wormhole.com/token.png',
     baseWager: 1e6,
     decimals: 6,
     usdPrice: 0,
