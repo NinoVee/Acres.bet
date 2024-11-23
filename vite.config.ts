e231752a64ad690d3d9b9eac0708687c7690c850
NINOVEE <109622508+NinoVee@users.noreply.github.com>
2024-11-23 11:50:54 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill'
-import rollupNodePolyfills from 'rollup-plugin-node-polyfills'
 
 const ENV_PREFIX = ['VITE_']
 
@@ -16,28 +13,6 @@ export default defineConfig(() => ({
   resolve: {
     alias: {
       crypto: 'crypto-browserify',
-      stream: 'stream-browserify', // Add polyfills for stream module
-      buffer: 'buffer',           // Add polyfills for buffer module
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['@raydium-io/raydium-sdk'], // Exclude SDK from bundling
-      plugins: [
-        rollupNodePolyfills(), // Polyfills for Node.js core modules in Rollup
-      ],
-    },
-  },
-  optimizeDeps: {
-    include: ['@raydium-io/raydium-sdk', 'crypto-browserify', 'buffer', 'stream-browserify'],
-    esbuildOptions: {
-      define: {
-        global: 'globalThis', // Define global for Node.js polyfills
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({ buffer: true }),
-        NodeModulesPolyfillPlugin(),
-      ],
     },
   },
   plugins: [
